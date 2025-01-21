ccf281fd2d2adc59297a87bbc2fd73ad7ab80ca0
Kamron <kamrosmith@gmail.com>
2025-01-20 19:21:05 -0800

    Renamed the autologin service because of Nix errors

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -111,6 +111,8 @@
   # Enable automatic login for the user.
    services.xserver.displayManager.autoLogin.enable = true;
   services.xserver.displayManager.autoLogin.user = "kam";
+   services.displayManager.autoLogin.enable = true;
+   services.displayManager.autoLogin.user = "kam";
 
   # Install firefox.
   programs.firefox.enable = true;
