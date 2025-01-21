6300854259dd153aa5ac7038f7da7da81a2d111a
Kamron <kamrosmith@gmail.com>
2025-01-20 17:33:27 -0800

    Setting up signing commits with 1password

--- home-manager/secret.nix
+++ home-manager/secret.nix
@@ -1,14 +1,27 @@
 { config, lib, pkgs, ... }:
 {
-  _: let
-    onePassPath = "~/.1password/agent.sock";
-  in {
-    programs.ssh {
-      enable = true;
-      extraConfig = ''
+  programs.ssh = {
+    enable = true;
+    extraConfig = ''
         Host *
-             IdentityAgent ${onePassPath}
+             IdentityAgent "~/.1password/agent.sock"
       '';
+  };
+
+  programs.git = {
+    extraConfig = {
+      gpg = {
+        format = "ssh";
+      };
+      "gpg \"ssh\"" = {
+        program = "${lib.getExe' pkgs._1password-gui "op-ssh-sign"}";
+      };
+      commit = {
+        gpgsign = true;
+      };
+      user = {
+        signingKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK87KQlTvH1gcGwsE0gW0wo7Er26Pgy3m7UDP+oTL7yC";
+      };
     };
   };
 }
