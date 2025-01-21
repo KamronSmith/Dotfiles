8736d8c9c77ee2bbc3bc77213ff98cb173cc5e28
Kamron <kamrosmith@gmail.com>
2025-01-20 19:20:49 -0800

    White space

--- home-manager/secret.nix
+++ home-manager/secret.nix
@@ -13,14 +13,26 @@
       gpg = {
         format = "ssh";
       };
+      
       "gpg \"ssh\"" = {
         program = "${lib.getExe' pkgs._1password-gui "op-ssh-sign"}";
       };
+      
       commit = {
         gpgsign = true;
       };
+      
       user = {
         signingKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK87KQlTvH1gcGwsE0gW0wo7Er26Pgy3m7UDP+oTL7yC";
+      };
+    };
+  };
+
+  programs.awscli = {
+    enable = true;
+    credentials = {
+      "default" = {
+        "credential_process" = "${pkgs._1password}/bin/op";
       };
     };
   };
