dc324839a4fc012e482ab9370589137e5599bbed
Kamron <kamrosmith@gmail.com>
2025-01-21 10:54:34 -0800

    commit again

--- nixos/scripts.nix
+++ nixos/scripts.nix
@@ -1,17 +1,10 @@
-{ pkgs, ... }:
+{ pkgs }:
 
-let
-    aws-op-cred-helper = pkgs.writeShellScriptBin "aws-op-cred-helper" '' 
+pkgs.writeShellScriptBin "aws-op-cred-helper" '' 
     cat <<END | ${pkgs._1password}/bin/op inject
      {
        "Version": 1,
        "AccessKeyId": "{{ op://Personal/AWS Access Key/aws_access_key_id }}",
        "SecretAccessKey": "{{ op://Personal/AWS Access Key/aws_secret_access_key }}",
      }
-    END '';
-
-  in {
-    environment.systemPackages = [
-      aws-op-cred-helper
-    ];
- }
+    END ''
