b910ded98024d74fbb962193860cd31b252b99b3
Kamron <kamrosmith@gmail.com>
2025-01-23 15:28:33 -0800

    Configured Nushell

--- home-manager/sh.nix
+++ home-manager/sh.nix
@@ -5,23 +5,44 @@
     nushell = {
       enable = true;
         extraConfig = ''
-          let carapace_completer = {|spans|
-          carapace $spans.0 nushell ...$spans | from json
-          }
-                $env.config = {
-                   show_banner: false,
-                   completions: {
-                   case_sensitive: false
-                   partial: true
-                   algorithm: "fuzzy"
-                   external: {
-                   enable: true
-                   max_results: 100
-                   completer: $carapace_completer
-                   }
-                   }
-                   edit_mode: "emacs"
-                                   }
+                    let carapace_completer = {|spans| carapace $spans.0 nushell ...$spans | from json }
+                   $env.config = {
+                               show_banner: false,
+                                completions: {
+                                             case_sensitive: false
+                                             partial: true
+                                             algorithm: "fuzzy"
+                                             external: {
+                                                       enable: true
+                                                       max_results: 100
+                                                       completer: $carapace_completer
+                                             }
+                                }
+
+                                edit_mode: emacs
+                                shell_integration: {
+                                                   osc2: true
+                                                   osc7: true
+                                                   osc8: true
+                                                   osc9_9: true
+                                                   osc133: true
+                                                   osc633: true
+                                                   reset_application_mode: true
+                                }
+
+                                buffer_editor: "emacsclient -c -a \"\""
+                                color_config: {
+                                              string: "#ffa07a"
+                                              variable: "#eedd82"
+                                }
+                                keybindings: [{
+                                             name: ctrl_backspace
+                                             modifier: control
+                                             keycode: backspace
+                                             mode: emacs
+                                             event: {edit: backspaceword }
+                                }]
+                  }
        '';
         environmentVariables = {
           EDITOR = "emacsclient -c -a \"\"";
