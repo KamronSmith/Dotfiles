ec7ac7f567e07024f9f2363d648cc221f9ae1716
Kamron <kamrosmith@gmail.com>
2025-01-23 15:28:05 -0800

    Configured Rofi

--- home-manager/rofi.nix
+++ home-manager/rofi.nix
@@ -1,8 +1,23 @@
-{ config, lib, pkgs }:
+{ config, lib, pkgs, ... }:
 
 {
   programs.rofi = {
     enable = true;
     package = pkgs.rofi-wayland;
+    location = "center";
+    theme = "/dev/null";
+    font = "Libre Caslon Regular";
+    extraConfig = {
+      display-drun = "Applications";
+      display-window = "Windows";
+      drun-display-format = "{icon} {name}";
+      modi = "window,run,drun";
+      show-icons = true;
+      border = 0;
+      margin = 0;
+      padding = 0;
+      spacing = 0;
+      width = "45%";
+    };
   };
 }
