93e5a246c35098df217570acb513086acb71c33c
Kamron <kamrosmith@gmail.com>
2025-01-31 16:45:22 -0800

    Customized waybar a lot

--- home-manager/programs/waybar.nix
+++ home-manager/programs/waybar.nix
@@ -11,21 +11,133 @@
         output = [
           "HDMI-A-4"
         ];
+        
         modules-left = [
-          "hyprland/workspaces"
-        ];
-        modules-right = [
-          "pulseaudio"
           "cpu"
           "memory"
+          "network"
+        ];
+        
+        modules-center = [
           "clock"
+          "hyprland/workspaces"
+        ];
+        
+        modules-right = [
+          "mpris"
+          "pulseaudio"
           "tray"
           "custom/power"
         ];
+        
         "hyprland/workspaces" = {
           active-only = true;
+          format = "{name}";
+          on-click = "activate";
+        };
+
+        "cpu" = {
+          interval = 5;
+          format = "  {usage}%";
+          max-length = 10;
+        };
+
+        "memory" = {
+          interval = 30;
+          format = "  {used:0.1f}GB";
+          format-alt = "  {}%";
+          max-length = 10;
+        };
+
+        "network" = {
+          format = "{icon}";
+          format-ethernet = "󰈁";
+          format-disconnected = "󰈂";
+        };
+
+        "clock" = {
+          format = "{:%H:%M %b %d %Y}";
+          format-alt = "{:%b %d %Y}";
+          tooltip-format = "<big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>";
+        };
+
+        "pulseaudio" = {
+          format = "{icon} {volume}%";
+          format-muted = "";
+          format-icons = {
+            default = [ "" "" "" ];
+          };
+          on-click = "pamixer -t";
+          on-scroll-up = "pamixer -i 1";
+          on-scroll-down = "pamixer -d 1";
+          on-click-right = "exec pavucontrol-qt";
+          tooltip-format = "Volume {volume}%";
+          scroll-step = 1;
+        };
+
+        "tray" = {
+          spacing = 10;
+        };
+
+        "mpris" = {
+          format = "{player_icon) {title}";
+          format-paused = " {status_icon} <i>{title}</i>";
+          max-length = 80;
+          player-icons = {
+            default = "▶";
+            mpv = "🎵";
+          };
+          status-icons = {
+            paused = "⏸";
+          };
         };
       };
     };
+
+    style = ''
+        * {
+          font-family: Iosevka Comfy, JetBrainsMono Nerd Font;
+          font-size: 17px;
+          border: none;
+          border-radius: 0;
+          min-height: 0;
+        }
+
+        window#waybar {
+          /* 26 27 38 0.5 */
+          background-color: #000000; 
+          color: #ffffff;
+          transition-property: background-color;
+          transition-duration: 0.5s
+        }
+
+        /* General styling for the individual modules */
+        #clock,
+        #cpu,
+        #memory,
+        #mpris,
+        #tray {
+          background-color: #222034;
+          font-size: 14px;
+          color: #8a909e;
+          padding: 3px 8px;
+          border-radius: 8px;
+          margin: 8px 2px;
+        }
+
+        #network,
+        #pulseaudio {
+          background-color: #222034;
+          font-size: 20px;
+          padding: 3px 8px;
+          margin: 8px 2px;
+          border-radius: 8px;
+        }
+
+        #cpu { color: #fb958b; }
+
+        #network.ethernet { color: #50C878; }
+        #network.disconnected { color: #90EE90; }
+      '';
   };
 }
