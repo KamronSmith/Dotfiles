e63266662734fd85d8defa404da993f634a8471c
Kamron <kamrosmith@gmail.com>
2025-01-31 16:45:42 -0800

    Added Gtk3 and QT themes

--- home-manager/themes.nix
+++ home-manager/themes.nix
@@ -7,5 +7,15 @@
       name = "Arc-dark";
       package = pkgs.arc-theme;
     };
+    gtk3 = {
+      extraConfig.gtk-application-prefer-dark-theme = true;
+    };
   };
+
+    qt = {
+      enable = true;
+      platformTheme.name = "gtk3";
+      style.name = "adwaita-dark";
+      style.package = pkgs.adwaita-qt;
+    };
 }
