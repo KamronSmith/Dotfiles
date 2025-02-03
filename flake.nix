eaa0c873d47fb5a2a88335ee0cbb22fbef059410
Kamron <kamrosmith@gmail.com>
2025-02-03 12:53:40 -0800

    FIREFOX: Added Firefox browser extensions as input to flake

--- flake.nix
+++ flake.nix
@@ -14,6 +14,11 @@
       url = "github:hyprwm/Hyprland";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    firefox-addons = {
+      url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = { self, nixpkgs, home-manager, ... } @inputs:
