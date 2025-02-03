ebb2ddac618972a0434a3d3641ae0bd2eb6b78ef
Kamron <kamrosmith@gmail.com>
2025-02-03 12:54:37 -0800

    FIRFOX: Added start to config

--- home-manager/programs/browsers.nix
+++ home-manager/programs/browsers.nix
@@ -1,7 +1,17 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   programs.firefox = {
     enable = true;
+    profiles.kam = {
+      search = {
+        force = true;
+      };
+
+      bookmarks = {};
+      settings = {
+        "browser.startup.homepage" = "about.home";
+      };
+    };
   };
 }
