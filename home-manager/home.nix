34b5420477b6f7a7090669042c4dea4f0a6f1578
Kamron <kamrosmith@gmail.com>
2025-02-03 19:34:47 -0800

    NIXPKGS: Trying to allow unfree packages

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -15,15 +15,12 @@
   ];
 
   nixpkgs = {
+    config.allowUnfree = true;
     # overlays = [
     #   outputs.overlays.additions
     #   outputs.overlays.modifications
     #   outputs.overlays.unstable-packages
     # ];
-
-    config = {
-      allowUnfree = true;
-    };
   };
   
   home.username = "kam";
