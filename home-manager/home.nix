057b5577c9d7cd832efeefd62203983e6689e93e
Kamron <kamrosmith@gmail.com>
2025-02-03 19:33:24 -0800

    GIT: Set 1password to be the git signing program

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -105,6 +105,9 @@
     enable = true;
     userName = "Kamron";
     userEmail = "kamrosmith@gmail.com";
+    extraConfig = {
+      gpg."ssh".program = "${pkgs._1password-gui}/bin/op-ssh-sign";
+    };
   };
   
 
