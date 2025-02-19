a6608635a2d32aec9acc4a43d74ed124570df5ce
Kamron <kamrosmith@gmail.com>
2025-02-18 17:48:59 -0800

    GIT: Made git into its own file

---
+++ home-manager/programs/git.nix
@@ -0,0 +1,13 @@
+{ config, pkgs, inputs }:
+
+{
+  programs.git = {
+    enable = true;
+    userName = "Kamron";
+    userEmail = "kamrosmith@gmail.com";
+    extraConfig = {
+      gpg."ssh".program = "${pkgs._1password-gui}/bin/op-ssh-sign";
+      pull.rebase = false;
+    };
+  };
+}
