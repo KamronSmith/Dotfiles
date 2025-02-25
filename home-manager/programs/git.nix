4085e4a83d04cc69d385d0415acb52165ee8809d
Kamron <kamrosmith@gmail.com>
2025-02-25 13:25:04 -0800

    GIT: Added a bunch of default Git settings

--- home-manager/programs/git.nix
+++ home-manager/programs/git.nix
@@ -6,8 +6,29 @@
     userName = "Kamron";
     userEmail = "kamrosmith@gmail.com";
     extraConfig = {
-      gpg."ssh".program = "${pkgs._1password-gui}/bin/op-ssh-sign";
-      pull.rebase = false;
+      gpg = {
+        "ssh".program = "${pkgs._1password-gui}/bin/op-ssh-sign";
+      };
+      
+      pull = {
+        rebase = false;
+      };
+      
+      color = {
+        ui = "auto";
+      };
+
+      push = {
+        default = "simple";
+      };
+
+      core = {
+        whitespace = "trailing-space,space-before-tab";
+      };
+
+      init = {
+        defaultBranch = "main";
+      };
     };
   };
 }
