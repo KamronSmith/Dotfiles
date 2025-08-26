5e26b25cf87c962a85a54ce63b20f2bed3c06b7f
Kamron Smith <kamrosmith@gmail.com>
2025-08-26 14:35:19 -0700

    PI: Added PI host to /etc/hosts

--- nixos/networking.nix
+++ nixos/networking.nix
@@ -8,6 +8,10 @@
 
     networkmanager.enable = true;
 
+    extraHosts = ''
+               10.0.0.108 pi
+    '';
+
     # proxy.default = "http://user:password@proxy:port/";
     # proxy.noProxy = "127.0.0.1,localhost,internal.domain";
   };
