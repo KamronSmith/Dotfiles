2045577021a2c6850820af6d8fdc67d43bbac9d7
Kamron Smith <kamronsmith@theBook-2.local>
2025-10-28 13:46:25 -0700

    PARROT: Modified install script to use symlinks

--- bin/parrot-install.sh
+++ bin/parrot-install.sh
@@ -11,8 +11,17 @@ mkdir ~/.config/nvim
 
 echo -e "Installing programs..."
 sudo apt install kitty -y
-wget https://raw.githubusercontent.com/KamronSmith/Dotfiles/refs/heads/master/config/tmux.conf -O ~/.config/tmux
-wget https://raw.githubusercontent.com/KamronSmith/Dotfiles/refs/heads/master/config/init.el -O ~/.config/nvim
-wget https://raw.githubusercontent.com/KamronSmith/Dotfiles/refs/heads/master/config/tmux-sessionizer.sh -O ~/tools/tmux-sessionizer.sh
+
+echo -e "Downloading configuration files..."
+git clone https://github.com/KamronSmith/dotfiles.git $HOME/tools/dotfiles
+
+echo -e "Creating symlinks..."
+ln -s $HOME/tools/dotfiles/config/init.lua ~/.config/nvim/init.lua
+ln -s $HOME/tools/dotfiles/config/tmux.conf ~/.config/tmux/tmux.conf
+ln -s $HOME/tools/dotfiles/bin/tmux-sessionizer.sh $HOME/tools/tmux-sessionizer.sh
+
+# wget https://raw.githubusercontent.com/KamronSmith/Dotfiles/refs/heads/master/config/tmux.conf -O ~/.config/tmux
+# wget https://raw.githubusercontent.com/KamronSmith/Dotfiles/refs/heads/master/config/init.el -O ~/.config/nvim
+# wget https://raw.githubusercontent.com/KamronSmith/Dotfiles/refs/heads/master/bin/tmux-sessionizer.sh -O ~/tools/tmux-sessionizer.sh
 
 echo -e "Setting path..."
