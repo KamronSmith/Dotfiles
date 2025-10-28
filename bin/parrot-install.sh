756b5d2fba8706c056e87bd8034eb99e7ccf8a46
Kamron Smith <kamronsmith@theBook-2.local>
2025-10-28 13:26:17 -0700

    PARROT: Updated install script

--- bin/parrot-install.sh
+++ bin/parrot-install.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
-
 set -xe
 
 pushd $HOME
 
+echo -e "Creating directories..."
 mkdir tools
 mkdir vpn
 mkdir ~/.config/tmux
 mkdir ~/.config/nvim
 
+echo -e "Installing programs..."
 sudo apt install kitty -y
-
 wget https://raw.githubusercontent.com/KamronSmith/Dotfiles/refs/heads/master/config/tmux.conf -O ~/.config/tmux
 wget https://raw.githubusercontent.com/KamronSmith/Dotfiles/refs/heads/master/config/init.el -O ~/.config/nvim
+wget https://raw.githubusercontent.com/KamronSmith/Dotfiles/refs/heads/master/config/tmux-sessionizer.sh -O ~/tools/tmux-sessionizer.sh
+
+echo -e "Setting path..."
