853ee55cd6d2c18c7c20c323f2a297f2a2073ae5
Kamron Smith <kamrosmith@gmail.com>
2026-03-26 15:21:07 -0700

    Added stow command to dotfiles module

--- .config/emacs/lisp/dotfiles.el
+++ .config/emacs/lisp/dotfiles.el
@@ -1,4 +1,9 @@
-
 (defvar kam-dotfiles-directory
   (concat (getenv "HOME")
           "/.dotfiles/"))
+
+(defun kam-dotfiles-stow ()
+  "Run stow in the dotfiles directory."
+  (interactive)
+  (let ((default-directory kam-dotfiles-directory))
+    (shell-command "stow .")))
