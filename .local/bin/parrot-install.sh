aa2a70ecbba823fa33501a165c72d2383e21a81a
Kamron Smith <kamrosmith@gmail.com>
2026-04-06 10:00:24 -0700

    .local: added scripts to dotfiles in .local/bin

---
+++ .local/bin/parrot-install.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+set -e
+pushd $HOME
+
+echo -e "Creating directories..."
+mkdir tools
+mkdir vpn
+mkdir ~/.config/tmux
+mkdir ~/.config/nvim
+mkdir ~/.config/kitty
+
+echo -e "Installing programs..."
+sudo apt install kitty -y
+
+wget https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.appimage -O ~/tools/nvim
+sudo chmod u+x ~/tools/nvim
+
+echo -e "Removing programs..."
+sudo apt remove neovim -y
+
+echo -e "Downloading configuration files..."
+git clone https://github.com/KamronSmith/dotfiles.git $HOME/tools/dotfiles
+
+echo -e "Creating symlinks..."
+ln -s $HOME/tools/dotfiles/config/init.lua ~/.config/nvim/init.lua
+ln -s $HOME/tools/dotfiles/config/tmux.conf ~/.config/tmux/tmux.conf
+ln -s $HOME/tools/dotfiles/bin/tmux-sessionizer.sh $HOME/tools/tmux-sessionizer.sh
+ln -s $HOME/tools/dotfiles/config/kitty.config ~/.config/kitty.conf
+
+# wget https://raw.githubusercontent.com/KamronSmith/Dotfiles/refs/heads/master/config/tmux.conf -O ~/.config/tmux
+# wget https://raw.githubusercontent.com/KamronSmith/Dotfiles/refs/heads/master/config/init.el -O ~/.config/nvim
+# wget https://raw.githubusercontent.com/KamronSmith/Dotfiles/refs/heads/master/bin/tmux-sessionizer.sh -O ~/tools/tmux-sessionizer.sh
+
+echo -e "Setting path..."
+echo "export PATH=~/tools:$PATH" >> ~/.bashrc
+source ~/.bashrc
