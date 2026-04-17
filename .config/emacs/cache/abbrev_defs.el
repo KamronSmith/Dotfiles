44dabe1518c5d2c32ebe055269c68b3942ee80ef
Kamron Smith <kamrosmith@gmail.com>
2026-04-17 00:01:28 -0700

    emacs: added abbrev definitions

---
+++ .config/emacs/cache/abbrev_defs.el
@@ -0,0 +1,8 @@
+;; -*- coding: utf-8; lexical-binding: t -*-
+(define-abbrev-table 'global-abbrev-table
+  '(
+    ("dont" "don't" nil :count 2)
+    ("teh" "the" nil :count 1)
+    ("thats" "that's" nil :count 2)
+   ))
+
