07528310cd3040c92e239c202694229291ad00cd
Kamron Smith <kamrosmith@gmail.com>
2026-04-25 14:47:45 -0700

    emacs: added old-mode

---
+++ .config/emacs/lisp/old-mode.el
@@ -0,0 +1,14 @@
+(define-minor-mode kam-old-mode
+  "Turn Emacs into an old application."
+  :global t
+  (if kam-old-mode
+      (progn
+        (menu-bar-mode 1)
+        (tool-bar-mode 1))
+    (menu-bar-mode -1)
+    (tool-bar-mode -1)))
+
+(standard-themes-with-colors
+  (custom-set-faces
+   `(menu ((,c :foreground "#505050" :background "#505050")))
+   `(tool-bar ((,c :foreground "#505050" :background "#505050")))))
