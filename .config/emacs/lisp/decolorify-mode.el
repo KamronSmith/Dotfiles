838b02c58ae2f49bf52117581a6dc1013d78b7d3
Kamron Smith <kamrosmith@gmail.com>
2026-06-21 00:17:03 -0700

    emacs: removed useless file

--- .config/emacs/lisp/decolorify-mode.el
+++
@@ -1,8 +0,0 @@
-;; -*- lexical-binding: t; -*-
-
-(define-minor-mode kam-decolorify-mode
-  "Turn off the colors."
-  :global t
-  (if kam-decolorify-mode
-      ()
-        (kam-standard-themes-reload-theme)))
