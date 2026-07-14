a1c818405953e047408a113331caa866afb7d061
Kamron Smith <kamrosmith@gmail.com>
2026-07-13 22:13:03 -0700

    emacs: writing-mode no longer disables tab-bar-mode

--- .config/emacs/lisp/writing-mode.el
+++ .config/emacs/lisp/writing-mode.el
@@ -6,11 +6,11 @@
       (progn
         (visual-fill-column-mode -1)
         (logos-focus-mode 1)
-        (tab-bar-mode -1)
+        ;; (tab-bar-mode -1)
         (kam-standard-themes-reload-theme)
         (hl-line-mode -1))
     (visual-fill-column-mode 1)
     (logos-focus-mode -1)
-    (tab-bar-mode 1)
+    ;; (tab-bar-mode 1)
     (kam-standard-themes-reload-theme)
     (hl-line-mode 1)))
