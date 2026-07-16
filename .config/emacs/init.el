c9da79aa4f42b0c9bb732ae3e53dec50843754a4
Kamron Smith <kamrosmith@gmail.com>
2026-07-15 20:43:43 -0700

    emacs: enable consult-hidden-buffer

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -36,7 +36,6 @@
   :config
   (add-to-list 'switch-to-prev-buffer-skip-regexp "\\*Async-native-compile-log\\*" t)
   (add-to-list 'switch-to-prev-buffer-skip-regexp "\\*Native-compile-Log\\*" t)
-  (add-to-list 'switch-to-prev-buffer-skip-regexp "\\*Compile-Log\\*" t)
 
   (with-eval-after-load 'consult
     (add-to-list 'consult-buffer-filter "\\*Async-native-compile-log\\*" t)
@@ -1385,7 +1384,7 @@ With non-nil optional argument DELIMITED, only replace matches surrounded by act
         '(consult-source-buffer
           consult-source-project-buffer
           consult-source-project-recent-file
-          ;; consult-source-hidden-buffer
+          consult-source-hidden-buffer
           ;; TODO 2026-07-15: figure out where neighbor file went
           ;; kam-consult-source-neighbor-file
           consult-source-bookmark
