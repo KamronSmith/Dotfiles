0fa93fb82e3d3f665ad8b3eda436934e33c87b35
Kamron Smith <kamrosmith@gmail.com>
2026-07-17 15:44:13 -0700

    hypr: fix windowing rules

--- .config/hypr/window-rules.lua
+++ .config/hypr/window-rules.lua
@@ -8,15 +8,15 @@ hl.window_rule({
         xwayland = true,
         float = true,
         fullscreen = false,
-        pin = false,
+        pin = false
     },
-    no_focus = true,
+    no_focus = true
 })
 
 hl.window_rule({
     name = "Firefox",
     match = { class = "^(firefox)$" },
-    workspace = 1,
+    workspace = 1
 })
 
 hl.window_rule({
@@ -29,33 +29,33 @@ hl.window_rule({
 hl.window_rule({
     name = "Suppress",
     match = { class = ".*" },
-    suppress_event = "maximize activate activatefocus",
+    suppress_event = "maximize activate activatefocus"
 })
 
 hl.window_rule({
     name = "Steam",
     match = { class = "^(steam)$", title = "^(Steam)$" },
-    workspace = 3,
+    workspace = 3
 })
 
 hl.window_rule({
     name = "Steam Games",
-    match = { class = "steam_app_" }, --maybe?
+    match = { class = "^(steam_app_[0-9]+)$" },
     fullscreen = true,
-    workspace = 3,
+    workspace = 4
 })
 
 hl.window_rule({
     name = "Idle Inhibit",
     match = { class = ".*" },
-    idle_inhibit = "fullscreen",
+    idle_inhibit = "fullscreen"
 })
 
 hl.window_rule({
     name = "Open File Dialog",
     match = { title = "^(Open File)(.*)$" },
     center = true,
-    float = true,
+    float = true
 })
 
 hl.window_rule({
@@ -69,21 +69,21 @@ hl.window_rule({
     name = "Choose Wallpaper Dialog",
     match = { title = "^(Choose wallpaper)(.*)$" },
     center = true,
-    float = true,
+    float = true
 })
 
 hl.window_rule({
     name = "Open Folder Dialog",
     match = { title = "^(Open Folder)(.*)$" },
     center = true,
-    float = true,
+    float = true
 })
 
 hl.window_rule({
     name = "Save As Dialog",
     match = { title = "^(Save As)(.*)$" },
     center = true,
-    float = true,
+    float = true
 })
 
 
@@ -91,7 +91,7 @@ hl.window_rule({
     name = "Library Dialog",
     match = { title = "^(Library)(.*)$" },
     center = true,
-    float = true,
+    float = true
 })
 
 
@@ -99,7 +99,7 @@ hl.window_rule({
     name = "File Upload Dialog",
     match = { title = "^(File Upload)(.*)$" },
     center = true,
-    float = true,
+    float = true
 })
 
 hl.window_rule({
@@ -109,26 +109,26 @@ hl.window_rule({
     -- move = "",
     size = { "(monitor_w*0.5)", "(monitor_h*0.5)" },
     float = true,
-    pin = true,
+    pin = true
 })
 
 hl.window_rule({
     name = "Volume Control",
     match = { title = "^(Volume Control)$" },
-    float = true,
+    float = true
 })
 
 
 hl.window_rule({
     name = "Bluetooth Devices",
     match = { title = "^(Bluetooth Devices)$" },
-    float = true,
+    float = true
 })
 
 hl.window_rule({
     name = "Anki",
     match = { class = "^(anki)$" },
-    float = true,
+    float = true
 })
 
 return window_rules
